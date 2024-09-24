2318bfeca4e8123f899970f6ef0b96d01b3ec288
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 03:28:09 +0800

    fix: rubocop offenses

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-  
+
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   protected
 
   def configure_permitted_parameters
-    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    added_attrs = [ :username, :email, :password, :password_confirmation, :remember_me ]
     devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
-    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :sign_in, keys: [ :login, :password ]
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
   end
 
