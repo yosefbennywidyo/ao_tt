1db15fdddfb630c888289a30c0e66e5303528ce8
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 03:23:37 +0800

    feat: setup devise parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,18 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
+  
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
   protected
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :password, :password_confirmation, :remember_me]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+    devise_parameter_sanitizer.permit :sign_in, keys: [:login, :password]
+    devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+  end
 
   def after_sign_in_path_for(resource)
     stored_location_for(resource) || me_url
