627465cd993a40bef0ed0a1115afe5fe7cfdcc15
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 02:26:13 +0800

    feat: setup after_sign_in_path to me_url

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   allow_browser versions: :modern
 
   before_action :authenticate_user!
+
+  protected
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || me_url
+  end
 end
