eaeaf9410bf188a71862c6afb30610fa4f4a7c42
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-24 23:47:40 +0800

    feat(routes): setup devise user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+    passwords: 'users/passwords'
+  }
   get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
