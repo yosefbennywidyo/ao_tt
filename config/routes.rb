47344ada6db7a56c580d8803bdab7403e8bc71da
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 00:24:41 +0800

    feat: setup routes, controller, and view for user list and me

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "users/index", as: 'user_list'
+  get "users/me", as: 'me'
+
   devise_for :users, controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations',
     confirmations: 'users/confirmations',
     passwords: 'users/passwords'
   }
-  get "home/index"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
@@ -15,7 +17,7 @@ Rails.application.routes.draw do
   # Render dynamic PWA files from app/views/pwa/*
   get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-
+  get "home/index"
   # Defines the root path route ("/")
   root "home#index"
 end
