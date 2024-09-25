a1275d4bbb051f6615954467b7350c2f004a7cf6
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 20:37:15 +0800

    fix: books controller
    - add current_book_author method
    - fix update and destroy method

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -36,24 +36,37 @@ class BooksController < ApplicationController
 
   # PATCH/PUT /books/1 or /books/1.json
   def update
-    respond_to do |format|
-      if @book.update(book_params)
-        format.html { redirect_to @book, notice: "Book was successfully updated." }
-        format.json { render :show, status: :ok, location: @book }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @book.errors, status: :unprocessable_entity }
+    if current_book_author
+      respond_to do |format|
+        if @book.update(book_params)
+          format.html { redirect_to @book, notice: "Book was successfully updated." }
+          format.json { render :show, status: :ok, location: @book }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @book.errors, status: :unprocessable_entity }
+        end
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to books_path, notice: "Only the book author allowed to update." }
+        format.json { head :no_content, status: :forbidden }
       end
     end
   end
 
   # DELETE /books/1 or /books/1.json
   def destroy
-    @book.destroy!
-
-    respond_to do |format|
-      format.html { redirect_to books_path, status: :see_other, notice: "Book was successfully destroyed." }
-      format.json { head :no_content }
+    if current_book_author
+      @book.destroy!
+      respond_to do |format|
+        format.html { redirect_to books_path, status: :see_other, notice: "Book was successfully destroyed." }
+        format.json { head :no_content }
+      end
+    else
+      respond_to do |format|
+        format.html { redirect_to books_path, notice: "Only the book author allowed to destroy." }
+        format.json { head :no_content, status: :forbidden }
+      end
     end
   end
 
@@ -61,6 +74,10 @@ class BooksController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_book
       @book = Book.find(params[:id])
+    end
+
+    def current_book_author
+      current_user.id == @book.author_id
     end
 
     # Only allow a list of trusted parameters through.
