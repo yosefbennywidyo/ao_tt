af1dc3004974c68a58bf2ccae1572ae19ac7386e
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 20:47:30 +0800

    refactor: book system test
    - update setup
    - update test

--- test/system/books_test.rb
+++ test/system/books_test.rb
@@ -2,42 +2,61 @@ require "application_system_test_case"
 
 class BooksTest < ApplicationSystemTestCase
   setup do
-    @book = books(:one)
+    @first_user = users(:one)
+    @second_user = users(:two)
+    @first_book = books(:first)
+    @second_book = books(:second)
+    sign_in @first_user
   end
 
   test "visiting the index" do
     visit books_url
     assert_selector "h1", text: "Books"
+    assert_selector "button[type="submit"]", text: "Create"
   end
 
   test "should create book" do
     visit books_url
     click_on "New book"
 
-    fill_in "Author", with: @book.author_id
-    fill_in "Description", with: @book.description
-    fill_in "Title", with: @book.title
+    fill_in "Title", with: @first_book.title
+    fill_in "Description", with: @first_book.description
+    select "Year of publication", '2024'
+    select "Author", '2024'
     click_on "Create Book"
 
     assert_text "Book was successfully created"
-    click_on "Back"
+    assert_redirected_to books_url
   end
 
   test "should update Book" do
-    visit book_url(@book)
+    visit book_url(@first_book)
     click_on "Edit this book", match: :first
 
-    fill_in "Author", with: @book.author_id
-    fill_in "Description", with: @book.description
-    fill_in "Title", with: @book.title
+    fill_in "Author", with: @first_book.author_id
+    fill_in "Description", with: @first_book.description
+    fill_in "Title", with: @first_book.title
     click_on "Update Book"
 
     assert_text "Book was successfully updated"
-    click_on "Back"
+    assert_redirected_to books_url
+  end
+
+  test "shouldn't update Book" do
+    visit book_url(@first_book)
+    click_on "Edit this book", match: :first
+
+    fill_in "Author", with: @first_book.author_id
+    fill_in "Description", with: ""
+    fill_in "Title", with: @first_book.title
+    click_on "Update Book"
+
+    assert_text "Description can't be blank"
+    assert_response :unprocessable_entity
   end
 
   test "should destroy Book" do
-    visit book_url(@book)
+    visit book_url(@first_book)
     click_on "Destroy this book", match: :first
 
     assert_text "Book was successfully destroyed"
