dd211aeee0e9a2128d4a968790afa363225a943f
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 09:29:03 +0800

    refactor(test): move sign_in to setup

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,16 +3,15 @@ require "test_helper"
 class UsersControllerTest < ActionDispatch::IntegrationTest
   setup do
     @user = users(:one)
+    sign_in @user
   end
 
   test "should get index" do
-    sign_in @user
     get user_list_url
     assert_response :success
   end
 
   test "should get me" do
-    sign_in @user
     get me_url
     assert_response :success
   end
