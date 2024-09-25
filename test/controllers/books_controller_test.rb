daa0410206ec7f8ae06c4daef4d55103a99c78ba
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-25 20:44:26 +0800

    refactor: update books controller test
    - add test when failed to create, update, and destroy book

--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -4,9 +4,10 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
   setup do
     @first_book = books(:first)
     @second_book = books(:second)
-    @user = users(:one)
+    @first_user = users(:one)
+    @second_user = users(:two)
     @author = Author.first
-    sign_in @user
+    sign_in @first_user
   end
 
   test "should get index" do
@@ -27,6 +28,26 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to books_url
   end
 
+  test "shouldn't create book" do
+    assert_difference -> { @author.books.count }, 0 do
+      post books_url, params: { book: { author_id: @author.id, description: "", title: "Book Title", year_of_publication: 2024 } }
+    end
+
+    assert_response :unprocessable_entity
+  end
+
+  test "shouldn't create book and show error messages" do
+    assert_difference -> { @author.books.count }, 0 do
+      post books_url, params: { book: { author_id: @author.id, description: "", title: "Book Title", year_of_publication: 2024 } }
+    end
+    error = assert_raises(ActiveModel::Error) do
+      post books_url, params: { book: { author_id: @author.id, description: "", title: "Book Title", year_of_publication: 2024 } }
+    end
+    assert_equal "Some error message", error.message
+
+    assert_response :unprocessable_entity
+  end
+
   test "should show book" do
     get book_url(@first_book)
     assert_response :success
@@ -42,8 +63,32 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to book_url(@first_book)
   end
 
+  test "shouldn't update book when book detail missing" do
+    patch book_url(@first_book), params: { book: { author_id: @author.id, description: "", title: "Book Title Updated" } }
+    assert_response :unprocessable_entity
+  end
+
+  test "shouldn't update book" do
+    sign_out @first_user
+    sign_in @second_user
+
+    patch book_url(@first_book), params: { book: { author_id: @author.id, description: "Book description updated", title: "Book Title Updated" } }
+    assert_redirected_to books_url
+  end
+
   test "should destroy book" do
     assert_difference("Book.count", -1) do
+      delete book_url(@first_book)
+    end
+
+    assert_redirected_to books_url
+  end
+
+  test "shouldn't destroy book" do
+    sign_out @first_user
+    sign_in @second_user
+
+    assert_difference("Book.count", 0) do
       delete book_url(@first_book)
     end
 
