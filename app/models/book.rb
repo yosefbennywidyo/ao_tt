19ae4f596c6e6f5bd230945137247952eeb8c808
Yosef Benny Widyokarsono <yosefbennywidyo@gmail.com>
2024-09-26 01:32:20 +0800

    feat: add send_notification after book created

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,10 @@ class Book < ApplicationRecord
   belongs_to :author
 
   validates_presence_of :title, :description, :year_of_publication
+
+  after_create :send_notification
+
+  def send_notification
+    BookMailerJob.perform_later(id, User.all.pluck(:id))
+  end
 end
